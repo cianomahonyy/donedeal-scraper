5e031c0ffea0dfc33001ac44749aff0612f31492
Cian O Mahony <CIAN@Cians-MacBook-Pro.local>
2019-02-17 22:28:27 +0000

    Re-coding the search function to allow people to search for cars more accurately

--- donedealCarScraper.py
+++ donedealCarScraper.py
@@ -6,19 +6,24 @@ headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KH
 page = 28
 
 # Here You Put Whatever Item You Want To Search For
-search = 'iphone 7'
+search = 'volkswagen golf'
 searchURL = search.replace(' ','%20')
 
 # Insert The Minimum and Maximum Values To Eliminate Outliers
-priceRange = ['100','800']
+priceRange = ['1000','40000']
+
+makeAndModel = ['Skoda','Yeti']
+
+yearRange = ['2015','2017']
 
 listOfPrices = []
+priceSlicer = []
 xPlot = []
 
 
 def priceFinder(page, searchURL, priceRange):
-	for i in range(1, 26):
-		url = 'https://www.donedeal.ie/all?words=' + searchURL + '&area=&campaign=14' + '&start=' + str(page) + '&price_from=' + priceRange[0] + '&price_to=' + priceRange[1]
+	for i in range(1, 40):
+		url = 'https://www.donedeal.ie/cars/' + makeAndModel[0] + '/' + makeAndModel[1] + '?start=' + str(page) + '&price_from=' + priceRange[0] + '&price_to=' + priceRange[1] + '&year_from=' + yearRange[0] + '&year_to=' + yearRange[1]
 		page = page + 28
 		print(url)
 		response = requests.get(url, headers=headers)
@@ -28,33 +33,38 @@ def priceFinder(page, searchURL, priceRange):
 
 		prices = soup.find_all('p', 'card__price')
 
+		pageNumber = soup.find_all('ng-class', 'icon-nav_arrow_left')
+
 		for price in prices:
 			itemPrice = price.get_text()
-			itemPrice = itemPrice[1:7]
+			itemPrice = itemPrice.replace("€","")
 			itemPrice = itemPrice.replace(",","")
 			if(itemPrice.isdigit()):
 				itemPrice = int(itemPrice)
-				listOfPrices.append(itemPrice)
-				print(itemPrice)
+				if(itemPrice < int(priceRange[1])):
+					listOfPrices.append(itemPrice)
 
 		print(listOfPrices)
 
 
-def xAxisMaker(xListOfPrices):
-	for x in range(0, len(xListOfPrices)):
+def xAxisMaker(listOfPrices):
+	for x in range(0, len(listOfPrices)):
 		xPlot.append(x)
 
 
 def getAverage(listOfPrices):
 	averagePrice = sum(listOfPrices) / float(len(listOfPrices))
 	averagePrice = round(averagePrice)
-	print('The average price of a ' + search + " is €" + str(averagePrice))
 	return averagePrice
+
 
 
 priceFinder(page, searchURL, priceRange)
 getAverage(listOfPrices)
 xAxisMaker(listOfPrices)
+
+print('The average price of a ' + search + " is €" + str(getAverage(listOfPrices)))
+print(len(listOfPrices))
 
 plt.scatter(xPlot, listOfPrices, label='skitscat', color='k')
 
@@ -63,3 +73,8 @@ plt.ylabel('price')
 plt.title(search + ' Scatter Plot')
 plt.legend()
 plt.show()
+
+
+
+
+
